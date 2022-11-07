9c65a8def4f0613bbcf0fd1b3360e1bd9247699c
Lucas Condon <lcondon@bennington.edu>
2022-11-07 12:11:02 -0500

    Add login flow

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
+import { createAuth0 } from '@auth0/auth0-vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+createApp(App).use(store).use(router).use(
+  createAuth0({
+    domain: process.env.VUE_APP_AUTH0_DOMAIN,
+    client_id: process.env.VUE_APP_AUTH0_CLIENT_ID,
+    redirect_uri: window.location.origin
+  })
+).mount('#app')
---
+++ ui/src/views/LoginView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <Login />
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from 'vue-class-component'
+import Login from '@/components/Login.vue' // @ is an alias to /src
+
+@Options({
+  components: {
+    Login
+  }
+})
+export default class HomeView extends Vue {}
+</script>
